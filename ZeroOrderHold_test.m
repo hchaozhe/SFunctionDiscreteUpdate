0be91bcfaaca0428941f494b93171d8dbb648cb7
hchaozhe <hchaozhe@umich.edu>
2017-11-21 19:31:34 -0500

    Add figure plot

--- ZeroOrderHold_test.m
+++ ZeroOrderHold_test.m
@@ -2,6 +2,8 @@
 clc;close all;clear all;
 %% It seems that by default there is this
 dT=0.1; % The sampling time here
-sim('ZeroOrderHold.mdl')
+sim('ZeroOrderHold.mdl',[0:0.001:10]);
 %%
 figure(1);hold on;grid on;
+plot(Hold.time,Hold.signals.values(:,1),'LineWidth',2);
+plot(Hold.time,Hold.signals.values(:,2),'r','LineWidth',2);
